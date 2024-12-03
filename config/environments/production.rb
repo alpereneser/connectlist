7063a51c9066f394f677e0c7c56a5f44c3759efe
Alperen Eser <alperen@connectlist.me>
2024-12-03 15:34:09 +0300

    Configure production environment for Railway deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,11 +15,15 @@ Rails.application.configure do
   # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
 
+  # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
+  config.assets.compile = true
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
